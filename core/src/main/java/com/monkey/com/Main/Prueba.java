a7802af802b0817c6a9f460bfd1b43d0382f85f2
JustPreo <impre0hn@gmail.com>
2025-09-30 15:03:50 -0600

    a

---
+++ core/src/main/java/com/monkey/com/Main/Prueba.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.monkey.com.Main;
+
+import com.badlogic.gdx.Screen;
+
+/**
+ *
+ * @author user
+ */
+public class Prueba implements Screen{
+
+    @Override
+    public void show() {
+       
+    }
+
+    @Override
+    public void render(float f) {
+        
+    }
+
+    @Override
+    public void resize(int i, int i1) {
+       
+    }
+
+    @Override
+    public void pause() {
+        
+    }
+
+    @Override
+    public void resume() {
+       
+    }
+
+    @Override
+    public void hide() {
+        
+    }
+
+    @Override
+    public void dispose() {
+    }
+    
+}
